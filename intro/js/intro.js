128f7545b79e5d0f18dd65d4eab55de15721a5a7
Isaac Colls <colls_isaac@hotmail.com>
2023-08-22 21:29:57 -0400

    feat: paradigma funcional

--- intro/js/intro.js
+++ intro/js/intro.js
@@ -1,1 +1,29 @@
+// paradigma funcional
 console.log("hi");
+// funciones de primer orden 👈️
+// En JavaScript, todas las funciones son funciones de primera clase.
+// Eso significa que pueden tratarse como cualquier otra variable.
+// Las funciones de primera clase son funciones que pueden asignarse como valores a variables,
+// devolverse desde otras funciones y pasarse como argumentos a otras funciones.
+
+function sum(a, b) {
+  return a + b;
+}
+
+let res = sum(1, 2);
+console.log(res);
+
+const fSum = sum;
+res = fSum(5, 6);
+console.log(res);
+
+// Las funciones de orden superior 👈️
+// en Java Script o cualquier otro lenguaje son aquellas
+// que reciben o retornan otras funciones
+
+function operation(fn, a, b) {
+  console.log("se hace algo");
+  console.log(fn(a, b));
+}
+
+operation(sum, 10, 20);
