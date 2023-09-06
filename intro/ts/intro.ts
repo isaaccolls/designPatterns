5d4eaf15e0f831fcaf686615c5aa0228dd7ac861
Isaac Colls <colls_isaac@hotmail.com>
2023-09-05 22:24:24 -0300

    feat: ts interfaz

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -14,19 +14,60 @@ class Drink {
 const drink = new Drink("fanta");
 console.log(drink.info());
 
-// herencia
-class Beer extends Drink {
-  private alcohol: number;
+// interfaz
+// permite categorizar objetos
+interface Product {
+  price: number;
+  getPrice(): string;
+}
 
-  constructor(name: string, alcohol: number) {
+// herencia
+class Beer extends Drink implements Product {
+  private alcohol: number;
+  price: number;
+
+  constructor(name: string, alcohol: number, price: number) {
     super(name);
     this.alcohol = alcohol;
+    this.price = price;
   }
 
   info(): string {
     return super.info() + " " + this.alcohol;
   }
+
+  getPrice(): string {
+    return "$ " + this.price;
+  }
 }
 
-const beer = new Beer("corona", 2.1);
+const beer = new Beer("corona", 2.1, 10);
 console.log(beer.info());
+
+class Snack implements Product {
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  getPrice(): string {
+    return "El precio es: $" + this.price;
+  }
+}
+
+const products: Product[] = [
+  new Beer("heineken", 4.5, 7),
+  new Snack("papas", 0.5),
+  new Beer("coors", 7.3, 13),
+];
+
+function getPrices(items: Product[]) {
+  for (const item of items) {
+    console.log(item.getPrice());
+  }
+}
+
+getPrices(products);
