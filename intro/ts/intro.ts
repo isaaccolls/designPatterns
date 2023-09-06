254b73c843d69c6c02e1850a0c6eca4b05673a3e
Isaac Colls <colls_isaac@hotmail.com>
2023-09-05 22:10:05 -0300

    feat: ts class

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,1 +1,15 @@
 console.log("hi typescript");
+
+class Drink {
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  info(): string {
+    return this.name;
+  }
+}
+
+const drink = new Drink("fanta");
+console.log(drink.info());
