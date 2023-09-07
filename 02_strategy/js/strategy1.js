df2527ca6c394f08b7a031d2d1391a0e42d03d86
Isaac Colls <colls_isaac@hotmail.com>
2023-09-06 22:31:35 -0300

    feat: strategy js another example

---
+++ 02_strategy/js/strategy1.js
@@ -0,0 +1,97 @@
+const data = [
+  {
+    name: "Erdinger Pikantus",
+    country: "Alemania",
+    info: "Erdinger Pikantus es una cerveza de estilo weizenbock elaborada en la localidad bávara de Erding.",
+    img: "https://dxjcdxuv6chk2.cloudfront.net/assets/biere/flascheglas/pikantus-2020-v2.png",
+  },
+  {
+    name: "Corona",
+    country: "México",
+    info: "La cerveza Corona es una marca mundialmente conocida, distribuida a lo largo de más de 159 países en los cinco continentes.",
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Corona-6Pack.JPG",
+  },
+  {
+    name: "Delirium Tremens",
+    country: "Bélgica",
+    info: "Esta pale ale tiene una efervescencia fina con un toque un tanto picante. Al tomarse, calienta el paladar y deja un sabor fuerte y de un amargor seco.",
+    img: "https://www.delirium.be/themes/custom/delirium/assets/img/beers/beer_delirium_tremens_bottle.png",
+  },
+];
+
+class InfoContext {
+  constructor(strategy, data, element) {
+    this.setStrategy(strategy);
+    this.data = data;
+    this.element = element;
+  }
+
+  setStrategy(strategy) {
+    this.strategy = strategy;
+  }
+
+  show() {
+    this.strategy.show(this.data, this.element);
+  }
+}
+
+class ListStrategy {
+  show(data, element) {
+    element.innerHTML = data.reduce((ac, beer) => {
+      return (
+        ac +
+        `<div>
+          <h2>${beer.name}</h2>
+          <p>${beer.country}</p>
+        </div>
+      <hr>`
+      );
+    }, "");
+  }
+}
+
+class Detailedtrategy {
+  show(data, element) {
+    element.innerHTML = data.reduce((ac, beer) => {
+      return (
+        ac +
+        `<div>
+          <h2>${beer.name}</h2>
+          <p>${beer.country}</p>
+          <p>${beer.info}</p>
+        </div>
+      <hr>`
+      );
+    }, "");
+  }
+}
+
+class ListWithImageStrategy {
+  show(data, element) {
+    element.innerHTML = data.reduce((ac, beer) => {
+      return (
+        ac +
+        `<div>
+          <h2>${beer.name}</h2>
+          <img width="10%" src="${beer.img}">
+        </div>
+      <hr>`
+      );
+    }, "");
+  }
+}
+
+const strategies = [
+  new ListStrategy(),
+  new Detailedtrategy(),
+  new ListWithImageStrategy(),
+];
+
+const info = new InfoContext(new ListStrategy(), data, content);
+info.show();
+
+slcOptions.addEventListener("change", (event) => {
+  const op = event.target.value;
+  info.setStrategy(strategies[op]);
+  info.show();
+});
