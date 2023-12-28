a42866f6efc4118b5d95b497f72efe937869e3db
Isaac Colls <colls_isaac@hotmail.com>
2023-12-27 22:26:48 -0300

    feat: state pattern on ts

---
+++ 06_state/ts/state.ts
@@ -0,0 +1,129 @@
+// Interface State
+interface State {
+  next(ticket: Ticket): number | null;
+  add(ticket: Ticket, quantity: number): void;
+}
+
+// Context
+class Ticket {
+  private state: State;
+  quantity: number;
+  readonly limit: number;
+  private number: number;
+
+  constructor(limit: number) {
+    this.limit = limit;
+    this.quantity = 0;
+    this.number = 0;
+    this.state = new EmptyState();
+  }
+
+  get getNumber(): number {
+    return this.number++;
+  }
+
+  set setState(state: State) {
+    this.state = state;
+  }
+
+  get getState(): State {
+    return this.state;
+  }
+
+  next(): number | null {
+    return this.state.next(this);
+  }
+
+  add(quantity: number): void {
+    this.state.add(this, quantity);
+  }
+}
+
+// ConcretStateA
+class EmptyState implements State {
+  next(ticket: Ticket): null {
+    return null;
+  }
+
+  add(ticket: Ticket, quantity: number): void {
+    if (quantity <= ticket.limit) {
+      ticket.quantity = quantity;
+      ticket.setState = new WithDataState();
+    } else if (quantity == ticket.limit) {
+      ticket.quantity = quantity;
+      ticket.setState = new FullState();
+    }
+  }
+}
+
+// ConcretStateB
+class WithDataState implements State {
+  next(ticket: Ticket): number | null {
+    ticket.quantity--;
+    if (ticket.quantity <= 0) {
+      ticket.setState = new EmptyState();
+    }
+    return ticket.getNumber;
+  }
+
+  add(ticket: Ticket, quantity: number): void {
+    if (ticket.quantity + quantity < ticket.limit) {
+      ticket.quantity += quantity;
+    } else if (ticket.quantity + quantity == ticket.limit) {
+      ticket.quantity += quantity;
+      ticket.setState = new FullState();
+    }
+  }
+}
+
+// ConcretStateC
+class FullState implements State {
+  next(ticket: Ticket): number | null {
+    ticket.quantity--;
+    if (ticket.quantity <= 0) {
+      ticket.setState = new EmptyState();
+    } else {
+      ticket.setState = new WithDataState();
+    }
+    return ticket.getNumber;
+  }
+
+  add(ticket: Ticket, quantity: number): void {
+    console.log("Ticket lleno!");
+  }
+}
+
+// ejecucion
+const ticket = new Ticket(5);
+console.log("ticket.getState:", ticket.getState);
+console.log("ticket.next():", ticket.next());
+//
+console.log("🐢 vamo a agrega 6");
+ticket.add(6);
+console.log("ticket.getState:", ticket.getState);
+console.log("ticket.next():", ticket.next());
+//
+console.log("🐢 vamo a agrega 4");
+ticket.add(4); // no esta lleno falta 1
+console.log("ticket.getState:", ticket.getState);
+console.log("ticket.next():", ticket.next());
+console.log("ticket.next():", ticket.next());
+//
+console.log("🐢 vamo a agrega 3");
+ticket.add(3); // con 3 queda lleno en 5
+console.log("ticket.getState:", ticket.getState);
+//
+console.log("🐢 vamo a agrega 1+");
+ticket.add(1); // no se agrega porque ya que esta lleno
+//
+console.log("🐢 vamo a quitar 1");
+console.log("ticket.next():", ticket.next());
+console.log("ticket.getState:", ticket.getState);
+//
+console.log("🐢 vamo a vacia");
+console.log("ticket.next():", ticket.next());
+console.log("ticket.next():", ticket.next());
+console.log("ticket.next():", ticket.next());
+console.log("ticket.next():", ticket.next());
+console.log("ticket.getState:", ticket.getState);
+console.log("ticket.next():", ticket.next());
