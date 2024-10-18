e23bbb292cf9e21a0290f9837b999f1a6adc7ac5
Isaac Colls <colls_isaac@hotmail.com>
2024-10-18 15:18:34 -0300

    feat: parse filters

--- 10_criteria/README.md
+++ 10_criteria/README.md
@@ -1,6 +1,7 @@
 # url example
 
 ```
+localhost:3000/api/users?filters[0][field]=name&filters[0][operator]=eq&filters[0][value]=rafa&orderBy=name&order=asc
 localhost:3000/api/users?filters[0][field]=name
                         &filters[0][operator]=eq
                         &filters[0][value]=rafa
---
+++ 10_criteria/criteria-example/src/app/api/users/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const filters = parseFilters(searchParams);
+  return NextResponse.json({
+    filters,
+    orderBy: searchParams.get("orderBy"),
+    order: searchParams.get("order"),
+  });
+}
+
+interface Filter {
+  field: string;
+  operator: string;
+  value: string;
+}
+
+function parseFilters(searchParams: URLSearchParams): Filter[] {
+  const tempFilters: Record<string, Partial<Filter>> = {};
+  searchParams.forEach((value, key) => {
+    const match = key.match(/filters\[(\d+)]\[(.+)]/);
+    if (match) {
+      const index = match[1];
+      const property = match[2] as keyof Filter;
+      if (!tempFilters[index]) {
+        tempFilters[index] = {};
+      }
+      tempFilters[index][property] = value;
+    }
+  });
+  return Object.values(tempFilters).filter(
+    (filter) =>
+      filter.field !== undefined &&
+      filter.operator !== undefined &&
+      filter.value !== undefined
+  ) as Filter[];
+}
